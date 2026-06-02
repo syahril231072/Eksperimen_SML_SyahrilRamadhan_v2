2d203ae5bafe4c38bb6c62ccf2d57a52b5ebd0bc
syahril ramadhan <syahril231072@gmail.com>
2026-06-02 07:49:52 +0000

    Fix MLflow CI workflow

--- preprocessing/automate_SyahrilRamadhan.py
+++ preprocessing/automate_SyahrilRamadhan.py
@@ -17,7 +17,7 @@ from sklearn.metrics import (
 # CONFIGURATION
 # ==========================================
 
-DATASET_PATH = "datanamadataset_raw/loan_approval_dataset.csv"
+DATASET_PATH = "namadataset_raw/loan_approval_dataset.csv"
 
 MODEL_PATH = "model.pkl"
 
